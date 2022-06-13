65edf6b09365cfe807cc5c099b282ad6d8db571f
Volodymyr Lysenko <tibank2071@gmail.com>
2022-06-13 17:57:52 +0300

    feat: implement user routes and services

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,5 +6,10 @@ const { responseMiddleware } = require('../middlewares/response.middleware');
 const router = Router();
 
 // TODO: Implement route controllers for user
+router.get('/', UserService.getAll, responseMiddleware);
+router.get('/:id', UserService.search, responseMiddleware);
+router.post('/', UserService.create, responseMiddleware);
+router.put('/:id', UserService.update, responseMiddleware);
+router.delete('/:id', UserService.delete, responseMiddleware);
 
 module.exports = router;
--- services/userService.js
+++ services/userService.js
@@ -1,16 +1,36 @@
 const { UserRepository } = require('../repositories/userRepository');
 
 class UserService {
+  // TODO: Implement methods to work with user
+  getAll() {
+    return UserRepository.getAll();
+  }
 
-    // TODO: Implement methods to work with user
+  create(data) {
+    const item = UserRepository.create(data);
+    return item;
+  }
 
-    search(search) {
-        const item = UserRepository.getOne(search);
-        if(!item) {
-            return null;
-        }
-        return item;
+  update(search, data) {
+    const item = UserRepository.update(search, data);
+    return item;
+  }
+
+  delete(search) {
+    try {
+      UserRepository.delete(search);
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  search(search) {
+    const item = UserRepository.getOne(search);
+    if (!item) {
+      return null;
+    }
+    return item;
+  }
 }
 
 module.exports = new UserService();
